6f16cb671ae6b107e90626020e9a990f9a4e63c1
Teslim Ibisomi <ibisomimayowa.com>
2023-12-22 19:39:52 +0100

    Product, Order and Payload configuration

--- src/index.ts
+++ src/index.ts
@@ -1,174 +1,225 @@
 import {
-  Record,
-  StableBTreeMap,
-  Vec,
-  Result,
-  nat64,
-  ic,
-  Opt,
-  Principal,
-} from 'azle';
-import { v4 as uuidv4 } from 'uuid';
+    Record,
+    StableBTreeMap,
+    Vec,
+    Result,
+    nat64,
+    ic,
+    Principal,
+    $update,
+    match,
+    $query,
+  } from 'azle';
+  import { v4 as uuidv4 } from 'uuid';
+  
+  // Define types for products, orders, and customer interactions
+  type FoodProduct = Record<{
+    id: string;
+    name: string;
+    description: string;
+    price: nat64;
+    quantityAvailable: number;
+  }>;
 
-// Define types for products, orders, and customer interactions
-type FoodProduct = {
-  id: string;
-  name: string;
-  description: string;
-  price: nat64;
-  quantityAvailable: number;
-  imageUrl: string;
-};
+  type FoodPayload = Record<{
+    name: string;
+    description: string;
+    price: nat64;
+    quantityAvailable: number;
+}>
+  
+  
+  type Order = Record<{
+    id: string;
+    customerId: string;
+    productId: string;
+    quantity: number;
+    status: string;
+    createdAt: nat64;
+  }>;
 
-type Order = {
-  id: string;
-  customerId: string;
-  productId: string;
-  quantity: number;
-  status: 'placed' | 'confirmed' | 'cancelled' | 'delivered';
-  createdAt: nat64;
-};
+  type OrderPayload = Record<{
+    customerId: string;
+    productId: string;
+    quantity: number;
+}>
+  
+  type CustomerInteraction = Record<{
+    id: string;
+    customerId: string;
+    productId: string;
+    rating: number;
+    review: string;
+    createdAt: nat64;
+  }>;
 
-type CustomerInteraction = {
-  id: string;
-  customerId: string;
-  productId: string;
-  rating: number;
-  review: string;
-  createdAt: nat64;
-};
+  type CustomerInteractionPayload = Record<{
+    customerId: string;
+    productId: string;
+    rating: number;
+    review: string;
+}>
+  
+  // Define storage for products, orders, and customer interactions
+  const foodProducts = new StableBTreeMap<string, FoodProduct>(0, 44, 512)
+  const orders =  new StableBTreeMap<string, Order>(1, 44, 512)
+  const customerInteractions = new StableBTreeMap<string, CustomerInteraction>(2, 44, 512)
+  
+  // set up with wallet of local user 
+  const owner: Principal = ic.caller();
+  
+  // Functions for managing products
+  $update;
+  export function addProduct(
+    payload: FoodPayload
+  ): Result<FoodProduct, string> {
+    const productId = uuidv4();
+    const product: FoodProduct = {
+      id: productId,
+      name: payload.name,
+      description: payload.description,
+      price: payload.price,
+      quantityAvailable : payload.quantityAvailable,
+    };
+    foodProducts.insert(product.id, product);
+    return Result.Ok(product);
+  }
+  
+  $update;
+  export function updateProductQuantity(productId: string, newQuantity: number): Result<FoodProduct, string>{
+    return match(foodProducts.get(productId), {
+      Some: (product) => {
+          if (owner.toString() !== ic.caller().toString()) {
+              return Result.Err<FoodProduct, string>("You are not the owner of this product");
+            }
+          const updatedProduct: FoodProduct = {...product, quantityAvailable: newQuantity};
+          foodProducts.insert(product.id, updatedProduct);
+          return Result.Ok<FoodProduct, string>(updatedProduct);
+      },
+      None: () => Result.Err<FoodProduct, string>(`Couldn't update Product with id=${productId}. Product not found`)
+  });
+  }
+  
+  //function to get all products by ID
+  $query;
+  export function getProductsbyId(): Result<Vec<FoodProduct>, string> {
+      return Result.Ok(foodProducts.values());
+  }
+  
+  // Functions for placing orders
+  $update;
+  export function placeOrder(
+    payload : OrderPayload
+  ): Result<Order, string>{
+    const orderId = uuidv4();
+    const order: Order = {
+      id: orderId,
+      customerId : payload.customerId,
+      productId: payload.productId,
+      quantity: payload.quantity,
+      status: 'placed',
+      createdAt: ic.time(),
+    };
+    orders.insert(orderId, order)
+    return Result.Ok(order)
+  }
+  
+  
+  //function for canceling order
+  $update;
+  export function cancelOrder(orderId: string): boolean {
+    return match(orders.get(orderId), {
+      Some: (order) => {
+        if (order && order.status === 'placed') {
+          order.status = 'cancelled';
+          return true; // Order cancelled successfully
+        }
+          return false; // Order not found or cannot be confirmed
+      },
+      None: () => {return false;}
+  });
+  }
+  
+  //Function for Confirming order
+  $update;
+  export function confirmOrder(orderId: string): boolean {
+    return match(orders.get(orderId), {
+      Some: (order) => {
+        if (order && order.status === 'placed') {
+          order.status = 'confirmed';
+          return true; // Order confirmed successfully
+        }
+          return false; // Order not found or cannot be confirmed
+      },
+      None: () => {return false;}
+  });
+  }
+  
+  //Function for delivering order
+  $query;
+  export function deliverOrder(orderId: string): boolean {
+    return match(orders.get(orderId), {
+      Some: (order) => {
+        if (order && order.status === 'confirmed') {
+          order.status = 'delivered';
+          return true; // Order delivered successfully
+        }
+        return false; // Order not found or cannot be delivered
+      },
+    None: () => {return false;}
+  });
+  }
+  
+  // Functions for managing customer interactions
+  $update;
+  export function addCustomerInteraction(
+    payload: CustomerInteractionPayload
+  ): Result<CustomerInteraction, string> {
+    const interactionId = uuidv4();
+    const interaction: CustomerInteraction = {
+      id: interactionId,
+      customerId: payload.customerId,
+      productId: payload.productId,
+      rating: payload.rating,
+      review: payload.review,
+      createdAt: ic.time(),
+    };
+    customerInteractions.insert(interactionId, interaction)
+    return Result.Ok(interaction)
+  }
+  
+  //Function for getting Customer's interactions
+  $query;
+  export function getCustomerInteractionsByProduct(productId: string): Result<Vec<CustomerInteraction>, string>{
+    // Return all customer interactions for a given product
+    return Result.Ok(customerInteractions.values().filter((interaction) => interaction.productId === productId));
+  }
 
-// Define storage for products, orders, and customer interactions
-const foodProducts: Record<string, FoodProduct> = {};
-const orders: Record<string, Order> = {};
-const customerInteractions: Record<string, CustomerInteraction> = {};
-
-// Functions for managing products
-export function addProduct(
-  name: string,
-  description: string,
-  price: nat64,
-  quantityAvailable: number,
-  imageUrl: string
-): FoodProduct {
-  const productId = uuidv4();
-  const product: FoodProduct = {
-    id: productId,
-    name,
-    description,
-    price,
-    quantityAvailable,
-    imageUrl,
+  // Functions for managing customer's orders
+  $query;
+  export function getOrdersByCustomer(customerId: string): Result<Vec<Order>, string> {
+    // return Object.values(orders).filter((order) => order.customerId === customerId);
+    return Result.Ok(orders.values().filter((order) => order.customerId === customerId));
+  }
+  
+  // Functions for managing products
+  $query;
+  export function getProducts(): Result<Vec<FoodProduct>, string> {
+    return Result.Ok(foodProducts.values());
+  }
+  
+  
+  
+  // A workaround to make the uuid package work with Azle
+  globalThis.crypto = {
+    getRandomValues: () => {
+      let array = new Uint8Array(32);
+  
+      for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 256);
+      }
+  
+      return array;
+    },
   };
-  foodProducts[productId] = product;
-  return product;
-}
-
-export function updateProductQuantity(productId: string, newQuantity: number): boolean {
-  const product = foodProducts[productId];
-  if (product) {
-    product.quantityAvailable = newQuantity;
-    return true; // Product quantity updated successfully
-  }
-  return false; // Product not found
-}
-
-export function getProductById(productId: string): FoodProduct | undefined {
-  return foodProducts[productId];
-}
-
-// Functions for managing orders
-export function placeOrder(
-  customerId: string,
-  productId: string,
-  quantity: number
-): Order {
-  const orderId = uuidv4();
-  const order: Order = {
-    id: orderId,
-    customerId,
-    productId,
-    quantity,
-    status: 'placed',
-    createdAt: ic.time(),
-  };
-  orders[orderId] = order;
-  return order;
-}
-
-export function cancelOrder(orderId: string): boolean {
-  const order = orders[orderId];
-  if (order && order.status === 'placed') {
-    order.status = 'cancelled';
-    return true; // Order cancelled successfully
-  }
-  return false; // Order not found or cannot be cancelled
-}
-
-export function confirmOrder(orderId: string): boolean {
-  const order = orders[orderId];
-  if (order && order.status === 'placed') {
-    order.status = 'confirmed';
-    return true; // Order confirmed successfully
-  }
-  return false; // Order not found or cannot be confirmed
-}
-
-export function deliverOrder(orderId: string): boolean {
-  const order = orders[orderId];
-  if (order && order.status === 'confirmed') {
-    order.status = 'delivered';
-    return true; // Order delivered successfully
-  }
-  return false; // Order not found or cannot be delivered
-}
-
-// Functions for managing customer interactions
-export function addCustomerInteraction(
-  customerId: string,
-  productId: string,
-  rating: number,
-  review: string
-): CustomerInteraction {
-  const interactionId = uuidv4();
-  const interaction: CustomerInteraction = {
-    id: interactionId,
-    customerId,
-    productId,
-    rating,
-    review,
-    createdAt: ic.time(),
-  };
-  customerInteractions[interactionId] = interaction;
-  return interaction;
-}
-
-export function getCustomerInteractionsByProduct(productId: string): CustomerInteraction[] {
-  // Return all customer interactions for a given product
-  return Object.values(customerInteractions).filter(
-    (interaction) => interaction.productId === productId
-  );
-}
-
-// Functions for managing orders
-export function getOrdersByCustomer(customerId: string): Order[] {
-  return Object.values(orders).filter((order) => order.customerId === customerId);
-}
-
-// Functions for managing products
-export function getProducts(): FoodProduct[] {
-  return Object.values(foodProducts);
-}
-
-// A workaround to make the uuid package work with Azle
-globalThis.crypto = {
-  getRandomValues: () => {
-    let array = new Uint8Array(32);
-
-    for (let i = 0; i < array.length; i++) {
-      array[i] = Math.floor(Math.random() * 256);
-    }
-
-    return array;
-  },
-};
+  
