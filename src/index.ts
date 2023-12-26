74175cae6811b5da623f971c7fdfc2e0dfa84b8b
Mayowa Teslim <129578879+Dolazdev@users.noreply.github.com>
2023-12-26 12:44:59 +0100

    fix update quantity, customer interaction and payloads.

--- src/index.ts
+++ src/index.ts
@@ -1,241 +1,322 @@
 import {
-    Record,
-    StableBTreeMap,
-    Vec,
-    Result,
-    nat64,
-    ic,
-    Principal,
-    $update,
-    match,
-    $query,
-  } from 'azle';
-  import { v4 as uuidv4 } from 'uuid';
-  
-  // Define types for products, orders, and customer interactions
-  type FoodProduct = Record<{
-    id: string;
-    name: string;
-    description: string;
-    price: nat64;
-    quantityAvailable: number;
-  }>;
+  Record,
+  StableBTreeMap,
+  Vec,
+  Result,
+  nat64,
+  ic,
+  Principal,
+  $update,
+  match,
+  $query,
+} from 'azle';
+import { v4 as uuidv4 } from 'uuid';
 
-  type FoodPayload = Record<{
-    name: string;
-    description: string;
-    price: nat64;
-    quantityAvailable: number;
+// Define types for products, orders, and customer interactions
+type FoodProduct = Record<{
+  productId: string;
+  name: string;
+  description: string;
+  price: nat64;
+  quantityAvailable: number;
+  author: Principal;
+}>;
+
+type FoodPayload = Record<{
+  name: string;
+  description: string;
+  price: nat64;
+  quantityAvailable: number;
 }>
-  
-  
-  type Order = Record<{
-    id: string;
-    customerId: Principal;
-    productId: string;
-    quantity: number;
-    status: string;
-    createdAt: nat64;
-  }>;
 
-  type OrderPayload = Record<{
-    productId: string;
-    quantity: number;
+type UpdatePayload = Record<{
+  productId: string;
+  newQuantity: number;
 }>
-  
-  type CustomerInteraction = Record<{
-    id: string;
-    customerId: Principal;
-    productId: string;
-    rating: number;
-    review: string;
-    createdAt: nat64;
-  }>;
 
-  type CustomerInteractionPayload = Record<{
-    productId: string;
-    rating: number;
-    review: string;
+
+type Order = Record<{
+  id: string;
+  customerId: Principal;
+  productId: string;
+  quantity: number;
+  status: string;
+  createdAt: nat64;
+}>;
+
+type getOrdersByCustomer = Record<{
+  customerId: string;
 }>
-  
-  // Define storage for products, orders, and customer interactions
-  const foodProducts = new StableBTreeMap<string, FoodProduct>(0, 44, 512)
-  const orders =  new StableBTreeMap<string, Order>(1, 44, 512)
-  const customerInteractions = new StableBTreeMap<string, CustomerInteraction>(2, 44, 512)
-  
-  // set up with wallet of local user 
-  const owner: Principal = ic.caller();
 
-  
-  // Query
-  //function to get all products by ID
-  $query;
-  export function getProductsbyId(id: string): Result<FoodProduct, string> {
-    return match(foodProducts.get(id), {
-      Some: (product) => Result.Ok<FoodProduct, string>(product),
-      None: () => Result.Err<FoodProduct, string>(`Product with id=${id} not found`)
-  });
+type OrderPayload = Record<{
+  productId: string;
+  quantity: number;
+}>
+
+type ConfirmOrderPayload = Record<{
+  productId: string;
+}>
+
+type getProductsbyId = Record<{
+  productId: string;
+}>
+
+type CustomerInteraction = Record<{
+  id: string;
+  customerId: Principal;
+  productId: string;
+  rating: number;
+  review: string;
+  createdAt: nat64;
+}>;
+
+type CustomerInteractionPayload = Record<{
+  productId: string;
+  rating: number;
+  review: string;
+}>
+
+type getCustomerInteractionsByProduct = Record<{
+  productId: string;
+}>
+
+// Define storage for products, orders, and customer interactions
+const foodProducts = new StableBTreeMap<string, FoodProduct>(0, 44, 512)
+const orders = new StableBTreeMap<string, Order>(1, 44, 512)
+const customerInteractions = new StableBTreeMap<string, CustomerInteraction>(2, 44, 512)
+
+// // set up with wallet of local user 
+// const owner: Principal = ic.caller();
+
+
+// $query;
+// export function productOwner(): string {
+//     return owner.toString();
+// }
+
+// Functions for managing products
+$update;
+export function addProduct(
+  payload: FoodPayload
+): Result<FoodProduct, string> {
+  const productId = uuidv4();
+  const product: FoodProduct = {
+    productId: productId,
+    name: payload.name,
+    description: payload.description,
+    price: payload.price,
+    quantityAvailable: payload.quantityAvailable,
+    author: ic.caller(),
+  };
+  foodProducts.insert(product.productId, product);
+  return Result.Ok(product);
+}
+
+//function for updating products
+$update;
+export function updateProductQuantity(payload: UpdatePayload): Result<FoodProduct, string> {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return Result.Err<FoodProduct, string>('Invalid Product ID provided.');
   }
 
-    //Function for getting Customer's interactions
-    $query;
-    export function getCustomerInteractionsByProduct(productId: string): Result<Vec<CustomerInteraction>, string>{
-      // Return all customer interactions for a given product
-      return Result.Ok(customerInteractions.values().filter((interaction) => interaction.productId === productId));
-    }
-  
-    // Functions for managing customer's orders
-    $query;
-    export function getOrdersByCustomer(customerId: string): Result<Vec<Order>, string> {
-      // return Object.values(orders).filter((order) => order.customerId === customerId);
-      return Result.Ok(orders.values().filter((order) => order.customerId.toString() === customerId));
-    }
-    
-    // Functions for managing products
-    $query;
-    export function getProducts(): Result<Vec<FoodProduct>, string> {
-      return Result.Ok(foodProducts.values());
-    }
+  return match(foodProducts.get(payload.productId), {
+    Some: (product) => {
+      // // Owner Authorization: Check if the caller is the owner
+      if (product.author.toString() !== ic.caller().toString()) {
+        return Result.Err<FoodProduct, string>('You are not the owner of this product');
+      }
+
+      // Update the product quantity
+      const updatedProduct: FoodProduct = { ...product, quantityAvailable: payload.newQuantity };
+
+      try {
+        foodProducts.insert(product.productId, updatedProduct);
+        return Result.Ok<FoodProduct, string>(updatedProduct);
+      } catch (error) {
+        return Result.Err<FoodProduct, string>(`Couldn't update Product with id=${payload.productId}. Product not found`);
+      }
+    },
+    None: () => Result.Err<FoodProduct, string>(`Couldn't update Product with id=${payload.productId}. Product not found`),
+  });
+}
 
 
-  // Update functions
+// Function to get all products by ID
+$query;
+export function getProductsbyId(payload: getProductsbyId): Result<FoodProduct, string> {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return Result.Err<FoodProduct, string>('Invalid ID provided.');
+  }
+
+  return match(foodProducts.get(payload.productId), {
+    Some: (product) => Result.Ok<FoodProduct, string>(product),
+    None: () => Result.Err<FoodProduct, string>(`Product with id=${payload.productId} not found`),
+  });
+}
+
+
+// Functions for placing orders
+$update;
+export function placeOrder(payload: OrderPayload): Result<Order, string> {
+  // Payload Validation: Ensure that required fields are present in the payload
+  if (!payload.productId || !payload.quantity) {
+    return Result.Err<Order, string>('Invalid payload');
+  }
+  return match(foodProducts.get(payload.productId  ), {
+    Some: (product) => {
+      // Order Authorization: Check if the caller is the owner
+      const orderId = uuidv4();
+      const order: Order = {
+        id: orderId,
+        customerId: ic.caller(),
+        productId: payload.productId,
+        quantity: payload.quantity,
+        status: 'placed',
+        createdAt: ic.time(),
+      };
+
+      // Insert the order into the orders map
+      orders.insert(orderId, order);
+
+      // Update the product quantity
+      const updatedProduct: FoodProduct = { ...product, quantityAvailable: product.quantityAvailable - payload.quantity };
+      foodProducts.insert(product.productId, updatedProduct);
+
+      return Result.Ok<Order, string>(order);
+    },
+    None: () => Result.Err<Order, string>('Product not found'),
+  })};
+
+
+//function for canceling order
+$update;
+export function cancelOrder(payload: ConfirmOrderPayload): boolean {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return false;
+  }
+  return match(orders.get(payload.productId), {
+    Some: (order) => {
+      // Owner Authorization: Check if the caller is the owner
+      if (order.customerId.toString() !== ic.caller().toString()) {
+        return false;
+      }
+      if (order && order.status === 'placed') {
+        order.status = 'cancelled';
+        return true; // Order cancelled successfully
+      }
+      return false; // Order not found or cannot be confirmed
+    },
+    None: () => { return false; }
+  });
+}
+
+//Function for Confirming order
+$update;
+export function confirmOrder(payload: ConfirmOrderPayload): boolean {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return false;
+  }
+  return match(orders.get(payload.productId), {
+    Some: (order) => {
+      // Owner Authorization: Check if the caller is the owner
+      if (order.customerId.toString() !== ic.caller().toString()) {
+        return false;
+      }
+      if (order && order.status === 'placed') {
+        order.status = 'confirmed';
+        return true; // Order confirmed successfully
+      }
+      return false; // Order not found or cannot be confirmed
+    },
+    None: () => { return false; }
+  });
+}
+
+//Function for delivering order
+$query;
+export function deliverOrder(payload: ConfirmOrderPayload): boolean {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return false;
+  }
+  return match(orders.get(payload.productId), {
+    Some: (order) => {
+      // Owner Authorization: Check if the caller is the owner
+      if (order.customerId.toString() !== ic.caller().toString()) {
+        return false;
+      }
+      if (order && order.status === 'confirmed') {
+        order.status = 'delivered';
+        return true; // Order delivered successfully
+      }
+      return false; // Order not found or cannot be delivered
+    },
+    None: () => { return false; }
+  });
+}
+
+// Functions for managing customer interactions
+$update;
+export function addCustomerInteraction(
+  payload: CustomerInteractionPayload
+): Result<CustomerInteraction, string> {
+  const interactionId = uuidv4();
+  const interaction: CustomerInteraction = {
+    id: interactionId,
+    customerId: ic.caller(),
+    productId: payload.productId,
+    rating: payload.rating,
+    review: payload.review,
+    createdAt: ic.time(),
+  };
+  customerInteractions.insert(interactionId, interaction)
+  return Result.Ok(interaction)
+}
+
+//Function for getting Customer's interactions
+$query;
+export function getCustomerInteractionsByProduct(payload: getCustomerInteractionsByProduct): Result<Vec<CustomerInteraction>, string> {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.productId) {
+    return Result.Err<Vec<CustomerInteraction>, string>('Invalid Product ID provided.');
+  }
+    // Return all customer interactions for a given product
+    return Result.Ok(customerInteractions.values().filter((interaction) => interaction.productId === payload.productId));
+  }
+
+  // Functions for managing customer's orders
+ // Functions for managing customer's orders
+$query;
+export function getOrdersByCustomer(payload: getOrdersByCustomer): Result<Vec<Order>, string> {
+  // Parameter Validation: Ensure that ID is provided
+  if (!payload.customerId) {
+    return Result.Err<Vec<Order>, string>('Invalid Customer ID provided.');
+  }
+
+  return Result.Ok(orders.values().filter((order) => order.customerId.toString() === payload.customerId));
+}
+
   // Functions for managing products
-  $update;
-  export function addProduct(
-    payload: FoodPayload
-  ): Result<FoodProduct, string> {
-    const productId = uuidv4();
-    const product: FoodProduct = {
-      id: productId,
-      name: payload.name,
-      description: payload.description,
-      price: payload.price,
-      quantityAvailable : payload.quantityAvailable,
-    };
-    foodProducts.insert(product.id, product);
-    return Result.Ok(product);
+  $query;
+  export function getProducts(): Result<Vec<FoodProduct>, string> {
+    return Result.Ok(foodProducts.values());
   }
-  
-  $update;
-  export function updateProductQuantity(productId: string, newQuantity: number): Result<FoodProduct, string>{
-    return match(foodProducts.get(productId), {
-      Some: (product) => {
-          if (owner.toString() !== ic.caller().toString()) {
-              return Result.Err<FoodProduct, string>("You are not the owner of this product");
-            }
-          const updatedProduct: FoodProduct = {...product, quantityAvailable: newQuantity};
-          foodProducts.insert(product.id, updatedProduct);
-          return Result.Ok<FoodProduct, string>(updatedProduct);
-      },
-      None: () => Result.Err<FoodProduct, string>(`Couldn't update Product with id=${productId}. Product not found`)
-  });
-  }
-  
-  
-  // Functions for placing orders
-  $update;
-  export function placeOrder(
-    payload : OrderPayload
-  ): Result<Order, string>{
-    return match(foodProducts.get(payload.productId), {
-      Some: (product) => {
-        const orderId = uuidv4();
-        const order: Order = {
-          id: orderId,
-          customerId : ic.caller(),
-          productId: payload.productId,
-          quantity: payload.quantity,
-          status: 'placed',
-          createdAt: ic.time(),
-        };
-        orders.insert(orderId, order)
-        const updatedProduct: FoodProduct = {...product, quantityAvailable: product.quantityAvailable - payload.quantity};
-        foodProducts.insert(product.id, updatedProduct);
-        return Result.Ok<Order, string>(order);
-      },
-      None: () => Result.Err<Order, string>(`Product not found`)
-  });
-  }
-  
-  
-  //function for canceling order
-  $update;
-  export function cancelOrder(orderId: string): boolean {
-    return match(orders.get(orderId), {
-      Some: (order) => {
-        if (order.customerId.toString() !== ic.caller().toString()) {
-          return false;
-        }
-        if (order && order.status === 'placed') {
-          order.status = 'cancelled';
-          return true; // Order cancelled successfully
-        }
-          return false; // Order not found or cannot be confirmed
-      },
-      None: () => {return false;}
-  });
-  }
-  
-  //Function for Confirming order
-  $update;
-  export function confirmOrder(orderId: string): boolean {
-    return match(orders.get(orderId), {
-      Some: (order) => {
-        if (order && order.status === 'placed') {
-          order.status = 'confirmed';
-          return true; // Order confirmed successfully
-        }
-          return false; // Order not found or cannot be confirmed
-      },
-      None: () => {return false;}
-  });
-  }
-  
-  //Function for delivering order
-  $update;
-  export function deliverOrder(orderId: string): boolean {
-    return match(orders.get(orderId), {
-      Some: (order) => {
-        if (owner.toString() !== ic.caller().toString()) {
-          return false;
-        }
-        if (order && order.status === 'confirmed') {
-          order.status = 'delivered';
-          return true; // Order delivered successfully
-        }
-        return false; // Order not found or cannot be delivered
-      },
-    None: () => {return false;}
-  });
-  }
-  
-  // Functions for managing customer interactions
-  $update;
-  export function addCustomerInteraction(
-    payload: CustomerInteractionPayload
-  ): Result<CustomerInteraction, string> {
-    const interactionId = uuidv4();
-    const interaction: CustomerInteraction = {
-      id: interactionId,
-      customerId: ic.caller(),
-      productId: payload.productId,
-      rating: payload.rating,
-      review: payload.review,
-      createdAt: ic.time(),
-    };
-    customerInteractions.insert(interactionId, interaction)
-    return Result.Ok(interaction)
-  }
-  
+
+
+
   // A workaround to make the uuid package work with Azle
   globalThis.crypto = {
     getRandomValues: () => {
       let array = new Uint8Array(32);
-  
+
       for (let i = 0; i < array.length; i++) {
         array[i] = Math.floor(Math.random() * 256);
       }
-  
+
       return array;
     },
   };
