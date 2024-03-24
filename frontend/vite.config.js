81dc28857eb30e8fb610001152e54a46ae920a53
Teslim Ibisomi <ibisomimayowa.com>
2024-03-24 12:25:34 +0100

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
-export default defineConfig({
+import react from '@vitejs/plugin-react';
+
+export default {
   plugins: [react()],
-})
+  build: {
+    // You can add custom Rollup options here
+    // For example, to customize the output directory:
+    outDir: 'dist',
+    // Or to configure external dependencies:
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-icons/fa'],
+    },
+  },
+};
