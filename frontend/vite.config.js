2c3e13737a771020a90ece0ea6f1359207ff6f75
Teslim Ibisomi <ibisomimayowa.com>
2024-03-29 23:39:25 +0100

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default {
   build: {
     rollupOptions: {
       // Specify external dependencies to prevent Rollup from bundling them
-      external: ['react-icons/fa']
+      external: ['react-icons']
     }
   }
 };
